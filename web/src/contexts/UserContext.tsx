52a6261c4c6465f7345a4a381d7f1cac88d9a350
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-20 19:50:32 -0300

    feat: create the context to hold userinfo like username, and balance

---
+++ web/src/contexts/UserContext.tsx
@@ -0,0 +1,47 @@
+import {
+  createContext,
+  useState,
+  useEffect,
+  ReactNode,
+} from "react";
+
+interface UserData {
+  balance: number;
+  username: string;
+  updateBalance: (newBalance: number) => void;
+  updateUsername: (newUsername: string) => void;
+}
+
+interface UserProviderProps {
+  children?: ReactNode;
+}
+
+export const UserContext = createContext({} as UserData);
+
+export function UserProvider({
+  children,
+}: UserProviderProps) {
+  const [username, setUsername] = useState("");
+  const [balance, setBalance] = useState(0);
+
+  function updateBalance(newBalance: number) {
+    setBalance(newBalance);
+  }
+
+  function updateUsername(newUsername: string) {
+    setUsername(newUsername);
+  }
+
+  return (
+    <UserContext.Provider
+      value={{
+        username,
+        updateBalance,
+        updateUsername,
+        balance,
+      }}
+    >
+      {children}
+    </UserContext.Provider>
+  );
+}
