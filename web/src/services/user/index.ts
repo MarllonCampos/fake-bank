331a7e4f3437343dfbdc28a8856ea5639f6138aa
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-20 19:50:09 -0300

    feat: create the service for creation of a user

---
+++ web/src/services/user/index.ts
@@ -0,0 +1,33 @@
+import { AccountObject } from "../../components/UserForm"
+import { saveCookie } from "../../utils/cookies"
+
+class User {
+  private route = "http://127.0.0.1:3000/user"
+  async createUser(user: AccountObject) {
+    // console.log({ user })
+    const headers = new Headers()
+    headers.append('content-type', 'application/json')
+    const res = await fetch(this.route, {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers
+    })
+    const { ok: requestSuccess } = res
+    const { token, ...data } = await res.json()
+
+    if (!requestSuccess) {
+      const { message } = data
+
+      throw ({ message, requestSuccess })
+    }
+
+    saveCookie(token, token)
+    const { user: userInfo } = data
+    return userInfo
+
+  }
+}
+
+
+
+export default new User()
