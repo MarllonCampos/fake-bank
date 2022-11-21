0896eef15cc4d8512982222f4a0019b55892e391
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-21 05:06:03 -0300

    feat: update react-router-dom and router components

--- web/src/Routes.tsx
+++ web/src/Routes.tsx
@@ -1,15 +1,41 @@
-import { Route, Routes } from "react-router-dom";
+import { ReactNode } from "react";
+import { Route, Routes, RouteProps, Navigate } from "react-router-dom";
+import { UserProvider } from "./contexts/UserContext";
+import Balance from "./pages/Balance";
+import Cashout from "./pages/Cashout";
 import CreateAccount from "./pages/CreateAccount";
 import Home from "./pages/Home";
+import MyAccount from "./pages/MyAccount";
+import { getCookie } from "./utils/cookies";
 
 export default function AppRoutes() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route
-        path="/create-account"
-        element={<CreateAccount />}
-      />
-    </Routes>
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create-account" element={<CreateAccount />} />
+        <Route
+          path="/myaccount"
+          element={
+            <RequireAuth>
+              <MyAccount />
+            </RequireAuth>
+          }
+        >
+          <Route index element={<Balance />} />
+          <Route path="cash-out" element={<Cashout />} />
+        </Route>
+      </Routes>
+    </UserProvider>
   );
+}
+
+function RequireAuth({ children }: { children: JSX.Element }) {
+  let token = getCookie();
+  console.log(token);
+  if (!token) {
+    return <Navigate to="/" replace />;
+  }
+
+  return children;
 }
