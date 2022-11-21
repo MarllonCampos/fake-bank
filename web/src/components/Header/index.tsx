d350bb108ee52454fe5b067de9dffabcd5ecff52
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-21 05:05:50 -0300

    feat: create header and signout option

--- web/src/components/Header/index.tsx
+++ web/src/components/Header/index.tsx
@@ -1,9 +1,27 @@
 import { Container } from "./styles";
-
+import { SignOut, CurrencyEth } from "phosphor-react";
+import { UserContext } from "../../contexts/UserContext";
+import { useContext } from "react";
+import { logoutCookie } from "../../utils/cookies";
+import { useNavigate } from "react-router-dom";
 function Header() {
+  const navigate = useNavigate();
+
+  function signoutAction() {
+    logoutCookie();
+    return navigate("/");
+  }
+  const { balance, username } = useContext(UserContext);
   return (
     <Container>
-      <h1>3</h1>
+      <div className="userInfo">
+        <h1 className="userInfo__username">{username}</h1>
+        <p className="userInfo__balance">
+          {balance}
+          <CurrencyEth size={48} className="currency" />
+        </p>
+      </div>
+      <SignOut className="signout" size={48} weight="bold" onClick={signoutAction} />
     </Container>
   );
 }
--- web/src/components/Header/styles.ts
+++ web/src/components/Header/styles.ts
@@ -1,5 +1,55 @@
 import styled from 'styled-components';
-
 export const Container = styled.div`
+  --gap:${props => props.theme.spacing.xxlarge};
+  --font-size: 48px;
+
   max-width: 800px;
+  width: 100%;
+  margin-inline: auto;
+  padding: ${props => props.theme.spacing.large} ${props => props.theme.spacing.xlarge};
+  border-radius: ${props => props.theme.radius.xlarge};
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: var(--gap);
+
+  background-color: ${props => props.theme.primary.dark};
+  color:${props => props.theme.gray[100]};
+  .userInfo {
+    display: flex;
+    align-items: center;
+    flex: 1;
+    justify-content: space-between;
+
+    &__username {
+      font-size: var(--font-size);
+    }
+
+    &__balance { 
+      font-size: var(--font-size);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      gap: ${props => props.theme.spacing.medium}
+    }
+    .currency {
+      width: var(--font-size);
+      height: var(--font-size);
+    }
+    
+  }
+  
+  .signout {
+    margin-left: auto;
+    width: var(--font-size);
+    height: var(--font-size);
+  }
+
+  @media (max-width: 790px) {
+    --font-size: 24px;
+    --gap: ${props => props.theme.spacing.large}
+  }
 `;
