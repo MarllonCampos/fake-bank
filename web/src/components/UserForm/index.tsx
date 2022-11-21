bdc66618992bca3fdb42a30d90c3a3ff66086b88
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-21 02:32:46 -0300

    feat: update UserForm to add more functionabilities

--- web/src/components/UserForm/index.tsx
+++ web/src/components/UserForm/index.tsx
@@ -1,15 +1,81 @@
-import React from "react";
+import { Eye, EyeClosed } from "phosphor-react";
+import React, { FormEvent, ChangeEvent, useState, useEffect, useMemo } from "react";
 import Button from "../Button";
 import Input from "../Input";
 
 import { Form } from "./styles";
 
-const UserForm: React.FC = () => {
+interface UserFormProps {
+  onSubmit: (object: AccountObject) => void;
+  buttonDisabled?: boolean;
+}
+
+export interface AccountObject {
+  username: string;
+  password: string;
+}
+interface FormElements extends HTMLFormControlsCollection {
+  username: HTMLInputElement;
+  password: HTMLInputElement;
+}
+const UserForm = ({ onSubmit, buttonDisabled, ...props }: UserFormProps) => {
+  const [isPasswordShowing, setIsPasswordShowing] = useState(false);
+  const [username, setUsername] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const disableSubmit = useMemo(
+    () => buttonDisabled || password.length === 0 || username.length === 0,
+    [password, username, buttonDisabled]
+  );
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const { username, password } = event.currentTarget.elements as FormElements;
+
+    onSubmit({
+      username: username.value,
+      password: password.value,
+    });
+  };
+
+  function handleUsernameInput(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+    setUsername(value);
+  }
+
+  function handlePasswordInput(event: ChangeEvent<HTMLInputElement>) {
+    const { value } = event.target;
+    setPassword(value);
+  }
+
+  function togglePasswordShowing() {
+    setIsPasswordShowing((prevState) => !prevState);
+  }
+
   return (
-    <Form className="userform">
-      <Input placeholder="Username" />
-      <Input type="password" placeholder="Password" />
-      <Button className="submit">Sign In</Button>
+    <Form className="userform" onSubmit={handleSubmit}>
+      <Input type="text" placeholder="Username" name="username" onChange={handleUsernameInput} value={username} />
+      <div className="password-holder">
+        <Input
+          type={isPasswordShowing ? "text" : "password"}
+          placeholder="Password"
+          name="password"
+          onChange={handlePasswordInput}
+        />
+        {isPasswordShowing ? (
+          <span className="eye-icon" onClick={togglePasswordShowing}>
+            <Eye size={20} className="eye-icon" />
+          </span>
+        ) : (
+          <span className="eye-icon" onClick={togglePasswordShowing}>
+            <EyeClosed size={20} />
+          </span>
+        )}
+      </div>
+      <Button type="submit" className="submit" disabled={disableSubmit}>
+        Sign In
+      </Button>
     </Form>
   );
 };
--- web/src/components/UserForm/styles.ts
+++ web/src/components/UserForm/styles.ts
@@ -7,15 +7,43 @@ export const Form = styled.form`
   margin-top: ${props => props.theme.spacing.large};
   
   
-  input + input {
-    margin-top: ${props => props.theme.spacing.large};
 
-  }
+  input {
+    &:selection{
+      background-color: transparent;
+    }
+
+    &:placeholder:selection {
+      background-color: transparent
+    }
+  } 
 
   .submit {
     margin-top: ${props => props.theme.spacing.xlarge};
-
+    &:selection{
+      background-color: transparent;
+      color: transparent
+    }
   }
 
+
+ .password-holder {
+    position: relative; 
+    margin-top: ${props => props.theme.spacing.large};
+    input {
+       width: 100%;
+    } 
+
+  } 
+
+  .eye-icon {
+    cursor: pointer;
+ 
+    position: absolute;
+    right: 0;
+    top: 50%;
+
+    transform: translate(-50%, -50%);
+  }
    
 `;
