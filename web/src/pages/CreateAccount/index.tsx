03b187e11a7b2bb28094ca9096367a30b0ed9092
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-21 05:07:17 -0300

    feat: change route name

--- web/src/pages/CreateAccount/index.tsx
+++ web/src/pages/CreateAccount/index.tsx
@@ -2,25 +2,20 @@ import React, { useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { Container, RightSideContainer } from "./styles";
 import LeftSideAccount from "../../components/LeftSideAccount";
-import UserForm, {
-  AccountObject,
-} from "../../components/UserForm";
+import UserForm, { AccountObject } from "../../components/UserForm";
 import phone from "../../assets/phone.png";
 import { Helmet } from "react-helmet";
 import User from "../../services/user";
 import { UserContext } from "../../contexts/UserContext";
 const CreateAccount: React.FC = () => {
   const navigate = useNavigate();
-  const { updateBalance, updateUsername } =
-    useContext(UserContext);
+  const { updateBalance, updateUsername } = useContext(UserContext);
   const handleSubmit = async (user: AccountObject) => {
     try {
-      const { balance, username } = await User.createUser(
-        user
-      );
+      const { balance, username } = await User.createUser(user);
       updateBalance(balance);
       updateUsername(username);
-      navigate("/my-account");
+      navigate("/myaccount");
     } catch (error) {
       console.log(error);
     }
@@ -38,14 +33,11 @@ const CreateAccount: React.FC = () => {
         <h1 className="title">&lt;&lt;NG.CASH&gt;&gt;</h1>
 
         <div className="login-box">
-          <h3 className="login-box__title">
-            Create an account
-          </h3>
+          <h3 className="login-box__title">Create an account</h3>
           <UserForm onSubmit={handleSubmit} />
 
           <p className="login-box__login">
-            Already have a account?{" "}
-            <Link to="/">Log Now</Link>
+            Already have a account? <Link to="/">Log Now</Link>
           </p>
         </div>
       </RightSideContainer>
--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -3,26 +3,26 @@ import { Link, useNavigate } from "react-router-dom";
 import { Container, RightSideContainer } from "./styles";
 import LeftSideAccount from "../../components/LeftSideAccount";
 import phoneTilted from "../../assets/phone-tilted.png";
-import UserForm, {
-  AccountObject,
-} from "../../components/UserForm";
+import UserForm, { AccountObject } from "../../components/UserForm";
 import User from "../../services/user";
 
 import { Helmet } from "react-helmet";
 import { UserContext } from "../../contexts/UserContext";
 const Home: React.FC = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const { updateBalance, updateUsername } =
-    useContext(UserContext);
+  const { updateBalance, updateUsername } = useContext(UserContext);
   const navigate = useNavigate();
   async function handleSubmit(user: AccountObject) {
     try {
+      setIsLoading(true);
       const { balance, username } = await User.login(user);
       updateBalance(balance);
       updateUsername(username);
-      navigate("/my-account");
+      navigate("/myaccount");
     } catch (error) {
       console.log(error);
+    } finally {
+      setIsLoading(false);
     }
   }
   return (
@@ -31,20 +31,14 @@ const Home: React.FC = () => {
         <title>Fake Bank | Login</title>
       </Helmet>
       <LeftSideAccount image={phoneTilted} />
-
       <RightSideContainer>
+        {isLoading && <p>Loadingup</p>}
         <h1 className="title">&lt;NG.CASH&gt;</h1>
         <div className="login-box">
-          <h3 className="login-box__title">
-            Log with your credentials
-          </h3>
-          <UserForm
-            onSubmit={handleSubmit}
-            buttonDisabled={false}
-          />
+          <h3 className="login-box__title">Log with your credentials</h3>
+          <UserForm onSubmit={handleSubmit} buttonDisabled={false} />
           <p className="login-box__create-account">
-            Don't have a account?{" "}
-            <Link to="create-account">Create Now</Link>
+            Don't have a account? <Link to="create-account">Create Now</Link>
           </p>
         </div>
       </RightSideContainer>
