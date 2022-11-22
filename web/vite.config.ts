106a1fc86f62a19e9633f0ae67dbb32c3dfa57b7
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2022-11-22 17:41:58 -0300

    feat: frontend update

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  preview: {
-    port: 4000
+
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(process.env.FRONTEND_PORT)
+  return {
+    plugins: [react()],
+    server: {
+      port
+    },
+    preview: {
+      port
+    }
   }
 })
