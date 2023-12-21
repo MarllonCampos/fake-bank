0d0044d5c032f801f953632fc464248295fced8a
Marllon Campos <52542050+MarllonCampos@users.noreply.github.com>
2023-12-20 23:51:38 -0300

    fix: add host 0.0.0.0 to vite config

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,19 +1,19 @@
-import { defineConfig, loadEnv } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 
-
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '')
-  const port = Number(process.env.FRONTEND_PORT) || Number(env.FRONTEND_PORT)
+  const env = loadEnv(mode, process.cwd(), '');
+  const port = Number(process.env.FRONTEND_PORT) || Number(env.FRONTEND_PORT);
   return {
     plugins: [react()],
     server: {
-      port
+      port,
+      host: '0.0.0.0',
     },
     preview: {
-      port
-    }
-  }
-})
+      port,
+    },
+  };
+});
